df7cae44563557231d5a42aa7bc4ae6143c21d64
Alberto Piras <alberto.piras@inpeco.com>
2017-06-29 09:58:47 +0200

    updating app.js

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 var fs = require('fs');
 
+/**
+ * Name of the file to parse
+ */
 var fileName = process.argv[2];
 
+/**
+ * Word to censure
+ */
 var wordToCensure = process.argv[3];
 
 var regex = RegExp('\\b' + wordToCensure + '\\b','g');
 
-
+/**
+ * Reading Stream
+ */
 var readable = fs.createReadStream(__dirname + '/' + fileName, {
     encoding: 'utf8',
     highWaterMark: 1 * 1024
 });
 
-var writable = fs.createWriteStream(__dirname + '/copy.txt');
+/**
+ * Writing Stream
+ */
+var writable = fs.createWriteStream(__dirname + '/result.txt');
 
 readable.on('data', function(chunk) {
 
-    chunk = chunk.replace(regex, "*****");
+    chunk = chunk.replace(regex, "***");
 
     writable.write(chunk);
 });
+
