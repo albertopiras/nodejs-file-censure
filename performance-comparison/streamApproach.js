a2855700b5d695d30085a7fd82b03178317781d1
Alberto Piras <alberto.piras@inpeco.com>
2017-07-11 17:29:24 +0200

    adding a module to read file with stream that returns a promise

---
+++ performance-comparison/streamApproach.js
@@ -0,0 +1,40 @@
+
+module.exports = function (fileName, regex) {
+
+    var fs = require('fs');
+    var promise = new Promise(function (resolve, reject) {
+        console.log("--------------------");
+        console.time('timer');
+        /**
+         * Reading Stream
+         */
+        var readable = fs.createReadStream(__dirname + '/' + fileName, {
+            encoding: 'utf8',
+            highWaterMark: 256 * 1024
+        });
+
+        /**
+         * Writing Stream
+         */
+        var writable = fs.createWriteStream(__dirname + '/streamOutput.txt');
+
+        readable.on('data', function (chunk) {
+            chunk = chunk.replace(regex, "***");
+            writable.write(chunk);
+        });
+
+        readable.on('end', function (err, data) {
+            if (err) {
+                console.log("Error during stream cens.");
+                reject(err)
+            }
+            else {
+                console.log("Stream Mode:");
+                console.timeEnd('timer');
+                resolve(data);
+            }
+        });
+    });
+
+    return promise;
+}
